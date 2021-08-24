911e195cf4ca95f40b64f02faf3eace473701b6a
Devang <devngc@gmail.com>
2021-08-24 13:34:00 +0530

    chrome extension final js

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -4,34 +4,43 @@ const inputBtn = document.querySelector("#input-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 const ulEl = document.getElementById("ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 } else {
   myLeads = [];
+}
+
+function render(leads) {
+  let listItem = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItem += `
+    <li>
+      <a href="${leads[i]}" target='_blank'>${leads[i]}</a>
+    </li>`;
+  }
+  ulEl.innerHTML = listItem;
 }
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = null;
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
 });
 
-function renderLeads() {
-  let listItem = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    listItem += `
-    <li>
-      <a href="${myLeads[i]}" target='_blank'>${myLeads[i]}</a>
-    </li>`;
-  }
-  ulEl.innerHTML = listItem;
-}
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 deleteBtn.addEventListener("dblclick", function () {
   myLeads = [];
   localStorage.clear();
-  renderLeads();
+  render(myLeads);
 });
