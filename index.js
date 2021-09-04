e1a27cb203b7495ac1c7778b7ff6a42895319972
Devang <devngc@gmail.com>
2021-09-04 22:03:53 +0530

    passenger done

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ let peopleCount = 0;
 
 function increment() {
   peopleCount += 1;
-  document.getElementById("count-el").innerHTML = peopleCount;
+  document.getElementById("count-el").textContent = peopleCount;
 }
 
 function save() {
-  console.log(peopleCount);
+  document.getElementById("save-el").textContent += " " + peopleCount + " - ";
+  peopleCount = 0;
+  document.getElementById("count-el").textContent = peopleCount;
 }
