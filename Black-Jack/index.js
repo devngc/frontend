7934ca9d6a5400d6f2798b14a8e08cbc01cb892c
Devang <devngc@gmail.com>
2021-09-12 13:43:35 +0530

    progress in JS

--- Black-Jack/index.js
+++ Black-Jack/index.js
@@ -1,20 +1,36 @@
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
-function startGame() {
+function renderGame() {
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "You've got Blackjack!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
 
-  console.log(message);
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  messageEl.textContent = message;
+}
+
+function startGame() {
+  renderGame();
+}
+
+function newCard() {
+  let card = 2;
+  sum += card;
+  renderGame();
 }
